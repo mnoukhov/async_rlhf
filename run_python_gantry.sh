bc5d94b33488032cb2d0821540e7dfe79d0eeada
Michael Noukhovitch <michaeln@allenai.org>
2026-03-05 17:37:27 +0000

    fix script and try new params

--- run_python_gantry.sh
+++ run_python_gantry.sh
@@ -5,12 +5,10 @@ IMAGE="ai2/cuda12.8-dev-ubuntu22.04-notorch"
 WEKA_MOUNT="oe-adapt-default:/weka/oe-adapt-default"
 DEFAULT_PYTHON="3.10"
 
-if [[ -z "$name" ]]; then
-  branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo no-git-branch)"
-  branch="${branch//\//-}"
-  ts="$(date -u +%Y%m%d-%H%M%S)"
-  name="${branch}-${ts}"
-fi
+branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo no-git-branch)"
+branch="${branch//\//-}"
+ts="$(date -u +%Y%m%d-%H%M%S)"
+name="${branch}-${ts}"
 
 gantry run \
   --beaker-image "$IMAGE" \
